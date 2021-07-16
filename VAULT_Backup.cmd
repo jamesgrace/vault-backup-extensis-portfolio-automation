8d07bb49e69e7f8e353c20d1da8545230aaabe67
James Grace <james.n.grace@gmail.com>
2021-07-16 09:09:24 -0400

    Update VAULT_Backup.cmd

--- VAULT_Backup.cmd
+++ VAULT_Backup.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-REM - - - - - - - - - - - - version 31july2018 - - -
+REM - - - - - - - - - - - - version 16july2021 - - -
 REM
 REM 
 REM 7-Zip (64bit) - http://www.7-zip.org
@@ -23,9 +23,9 @@ set backuplogfilename=VAULT_Backup_Log.txt
 
 set emailserver=celartem-com.mail.protection.outlook.com
 
-set emailto=JGrace@extensis.com
+set emailto=portfolio.admin@example.com
 
-set emailfrom=Playground_noreply@extensis.com
+set emailfrom=Portfolio_noreply@example.com
 
 set emailsubject="Portfolio Notification: VAULT Backup Log"
 
@@ -42,8 +42,6 @@ For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set thetime=%%a%%b)
 set foldername=vault_%thedate%_T%thetime%
 
 > "%backupfolder%\%backuplogfilename%" 2>&1 (
-
-REM "C:\Program Files (x86)\Extensis\Portfolio Server\applications\jre\bin\java.exe" -cp "C:\Program Files (x86)\Extensis\Portfolio Server\applications\lib\portfolio-utilities.jar;C:\Program Files (x86)\Extensis\Portfolio Server\applications\lib\dam-client.jar;C:\Program Files (x86)\Extensis\Portfolio Server\applications\lib\jettison-1.3.3.jar;C:\Program Files (x86)\Extensis\Portfolio Server\applications\lib\jersey-core-1.17.1.jar;C:\Program Files (x86)\Extensis\Portfolio Server\applications\lib\jersey-client-1.17.1.jar;C:\Program Files (x86)\Extensis\Portfolio Server\applications\lib\json-simple-1.1.1.jar" -Xmx512m extensis.portfolio.clUtilities.PortfolioUtilitiesMain cleanupvault -p Portfolio2018 -noprompt
 
 mkdir "%backupfolder%\%foldername%"
 
